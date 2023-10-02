567e8111adb5031692a7305cf6c96444486612ad
markwylde-deriv <markwylde-deriv@users.noreply.github.com>
2023-10-02 10:52:34 +0000

    Deploying to gh-pages from @ deriv-experiments/web-demo-with-release@e5852022443991182752f98a846f58051445ea1b 🚀

---
+++ e5852022443991182752f98a846f58051445ea1b/index.js
@@ -0,0 +1,9 @@
+document.body.innerHTML=`
+  <h1>Test Website</h1>
+  This is a test website
+
+  <footer>
+    preview-e5852022443991182752f98a846f58051445ea1b
+  </footer>
+`;
+//# sourceMappingURL=index.js.map
