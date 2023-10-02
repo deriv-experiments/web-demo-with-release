88a16a47c1a8f980ea602f3163a68393b24bf181
markwylde-deriv <markwylde-deriv@users.noreply.github.com>
2023-10-02 10:37:45 +0000

    Deploying to gh-pages from @ deriv-experiments/web-demo-with-release@0e430be241f4a606eb2e0e5a3fa5488a62905671 🚀

--- v1.0.0/index.js
+++ v1.0.0/index.js
@@ -1,2 +1,9 @@
-document.body.innerHTML="Hello World";
+document.body.innerHTML=`
+  <h1>Hello</h1>
+  Hello to the world
+
+  <footer>
+    vv1.0.0
+  </footer>
+`;
 //# sourceMappingURL=index.js.map
