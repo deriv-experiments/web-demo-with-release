30e0130a509f55517bc722af1584ba5d638d957e
markwylde-deriv <markwylde-deriv@users.noreply.github.com>
2023-10-02 10:33:22 +0000

    Deploying to gh-pages from @ deriv-experiments/web-demo-with-release@b0f096915ff7be50872befd6c58e0d9b743227b4 🚀

---
+++ b0f096915ff7be50872befd6c58e0d9b743227b4/index.js
@@ -0,0 +1,9 @@
+document.body.innerHTML=`
+  <h1>Hello</h1>
+  Hello to the world
+
+  <footer>
+    vunreleased-b0f096915ff7be50872befd6c58e0d9b743227b4
+  </footer>
+`;
+//# sourceMappingURL=index.js.map
